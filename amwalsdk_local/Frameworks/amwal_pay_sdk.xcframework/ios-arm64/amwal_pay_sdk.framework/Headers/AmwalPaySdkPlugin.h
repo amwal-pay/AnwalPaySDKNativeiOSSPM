0e378da4cbd8c9b6e367b2e3a6b0bf696945794e
Amr Said <amr.elskaan@amwal-pay.com>
2026-05-08 03:18:53 +0300

    Switch example app to local SPM; gitignore xcframeworks
    - Xcode project uses XCLocalSwiftPackageReference (relativePath=".") so
      the example app resolves the SDK from the local repo without hitting
      GitHub. Xcframeworks are copied locally by the developer (not in git).
    - .gitignore excludes amwalsdk_local/Frameworks/ (too large for git,
      Flutter.xcframework dSYM alone is 117 MB).
    - Remote Package.swift (url: targets) remains at tag 1.1.91 for
      consumers who add it from GitHub once the repo is made public.

--- amwalsdk_local/Frameworks/amwal_pay_sdk.xcframework/ios-arm64/amwal_pay_sdk.framework/Headers/AmwalPaySdkPlugin.h
+++
@@ -1,5 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface AmwalPaySdkPlugin : NSObject<FlutterPlugin>
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar;
-@end
--- amwalsdk_local/Frameworks/amwal_pay_sdk.xcframework/ios-arm64/amwal_pay_sdk.framework/Headers/AmwalSDK.h
+++
@@ -1,7 +0,0 @@
-#import "AmwalPaySdkPlugin.h"
-#import <Flutter/Flutter.h>
-
-// Main SDK header file
-@interface AmwalSDK : NSObject<FlutterPlugin>
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar;
-@end
